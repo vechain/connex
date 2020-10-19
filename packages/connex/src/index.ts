3aec2a194d6b7111cb8b393a136c263f3877260f
qianbin <cola.tin.com@gmail.com>
2020-10-19 14:55:08 +0800

    connex: compat for connex v1

--- packages/connex/src/index.ts
+++ packages/connex/src/index.ts
@@ -2,6 +2,7 @@ import { Framework } from '@vechain/connex-framework'
 import { SimpleNet } from '@vechain/connex-driver/dist/simple-net'
 import { genesisBlocks } from './config'
 import { Driver } from './driver'
+import { compat1 } from './compat'
 
 export import Thor = Connex.Thor
 export import Vendor = Connex.Vendor
@@ -30,8 +31,11 @@ export function create(nodeUrl: string, opts?: Options): Connex {
     }
 
     try {
-        const injected = ((window || {}) as any).connex as Connex
+        const injected = ((window || {}) as any).connex
         if (injected && injected.thor.genesis.id === genesis.id) {
+            if (/^1\./.test(injected.version)) {
+                return compat1(injected)
+            }
             return injected
         }
     } catch { /**/ }
