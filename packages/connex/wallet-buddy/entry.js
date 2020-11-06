82ca21b02cf06c2a22d06efc17c58081fa42a7a8
qianbin <cola.tin.com@gmail.com>
2020-11-06 16:02:53 +0800

    buddy: fix incorrect version print

--- packages/connex/wallet-buddy/entry.js
+++ packages/connex/wallet-buddy/entry.js
@@ -1,4 +1,4 @@
 export * from './esm/index'
 
-import pkg from '../package.json'
-console.log(`[ConnexWalletBuddy v${pkg.version}] loaded`)
+import pkg from './package.json'
+console.log(`[${pkg.name}] v${pkg.version} loaded`)
