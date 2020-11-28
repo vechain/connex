0075e6b8bc63eed1823e301e79c62f391896d737
qianbin <cola.tin.com@gmail.com>
2020-11-29 01:04:22 +0800

    connex: remove head cache logic

--- packages/connex/src/driver.ts
+++ packages/connex/src/driver.ts
@@ -7,8 +7,8 @@ const BUDDY_LIB_NAME = 'ConnexWalletBuddy'
 
 class Driver extends DriverNoVendor {
     private readonly buddy: Promise<ReturnType<typeof ConnexWalletBuddy.create>>
-    constructor(nodeUrl: string, genesis: Connex.Thor.Block, initHead: Connex.Thor.Status['head'] | undefined, readonly spaWalletUrl: string) {
-        super(new SimpleNet(nodeUrl), genesis, initHead)
+    constructor(nodeUrl: string, genesis: Connex.Thor.Block, readonly spaWalletUrl: string) {
+        super(new SimpleNet(nodeUrl), genesis)
         this.buddy = loadLibrary<typeof ConnexWalletBuddy>(
             BUDDY_SRC,
             BUDDY_LIB_NAME
@@ -32,22 +32,8 @@ export function create(nodeUrl: string, genesis: Connex.Thor.Block, spaWalletUrl
     })
     let driver = cache[key]
     if (!driver) {
-        const headCacheKey = `connex-head-${genesis.id}@${nodeUrl}`
-        let head
-        try {
-            const cachedHead = JSON.parse(localStorage.getItem(headCacheKey) || '') as Connex.Thor.Status['head']
-            // use the cachedHead if not too old
-            if (cachedHead.timestamp * 1000 > Date.now() - 3600 * 1000) {
-                head = cachedHead
-            }
-        } catch { /** */ }
-        cache[key] = driver = new Driver(nodeUrl, genesis, head, spaWalletUrl)
 
-        window.addEventListener('pagehide', () => {
-            if (driver.head.timestamp > Date.now() - 300 * 1000) {
-                localStorage.setItem(headCacheKey, JSON.stringify(driver.head))
-            }
-        })
+        cache[key] = driver = new Driver(nodeUrl, genesis, spaWalletUrl)
     }
     return driver
 }
