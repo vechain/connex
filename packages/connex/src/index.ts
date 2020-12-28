474043fa1acb97a840fc1195f2dae2b119990d36
qianbin <cola.tin.com@gmail.com>
2020-12-28 13:29:16 +0800

    connex: remove connex.vendor target; embed Vendor class in Connex namespace

--- packages/connex/src/index.ts
+++ packages/connex/src/index.ts
@@ -1,8 +1,8 @@
 import { Framework } from '@vechain/connex-framework'
 import { genesisBlocks } from './config'
 import { compat1 } from './compat'
-import { create as createDriver } from './driver'
-
+import { create as createDriver, DriverVendorOnly } from './driver'
+import { newVendor } from '@vechain/connex-framework'
 
 function normalizeNetwork(n: Options['network']) {
     n = n || 'main'
@@ -17,12 +17,50 @@ function normalizeNetwork(n: Options['network']) {
     }
 }
 
+function normalizeGenesisId(id?: 'main' | 'test' | string) {
+    id = id || 'main'
+    if (/^0x[0-9a-f]{64}$/.test(id)) {
+        return id
+    }
+    const gb = genesisBlocks[id as 'main' | 'test']
+    if (gb) {
+        return gb.id
+    }
+    throw new Error('invalid genesis id')
+}
+
+class VendorClass implements Connex.Vendor {
+    sign !: Connex.Vendor['sign']
+    constructor(genesisId?: 'main' | 'test' | string) {
+        genesisId = normalizeGenesisId(genesisId)
+        try {
+            const injected = ((window || {}) as any).connex
+            if (injected && injected.thor.genesis.id === genesisId) {
+                if (/^1\./.test(injected.version)) {
+                    return compat1(injected).vendor
+                }
+                return injected.vendor
+            }
+        } catch { /**/ }
+        const driver = new DriverVendorOnly(genesisId)
+        const vendor = newVendor(driver)
+        return {
+            get sign() {
+                // eslint-disable-next-line @typescript-eslint/unbound-method
+                return vendor.sign
+            }
+        }
+    }
+}
+
 export type Options = {
     nodeUrl: string
     network?: 'main' | 'test' | Connex.Thor.Block
 }
 
 class ConnexClass implements Connex {
+    static readonly Vendor = VendorClass
+
     thor!: Connex.Thor
     vendor!: Connex.Vendor
 
